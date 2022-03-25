7d804ca53aff66204ebb5f29c8cff2bc505ea2d5
ezn6 <kcourtney@naver.com>
2022-03-25 18:23:17 +0900

    no message

--- 기본/2577.js
+++ 기본/2577.js
@@ -1,20 +1,16 @@
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let input = fs.readFileSync(filePath).toString().split('\n');
-const re = input[0] * input[1] * input[2];
+const total = input[0] * input[1] * input[2];
 
 // console.log(String(re));
 //17037300
 
-solution(String(re));
-function solution(re) {
-  let arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+solution(String(total));
+function solution(total) {
+  let re = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
   for (let i = 0; i <= 9; i++) {
-    for (let j = 0; j <= 9; j++) {
-      if (re[j] == i) {
-        arr[i] += 1;
-      }
-    }
+    re[total[i]] += 1;
   }
-  console.log(arr.join('\n'));
+  console.log(re.join('\n'));
 }
